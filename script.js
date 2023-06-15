209ca1802b7183bb0b971475e01760ad609baabe
FelipeEnne <felipeenne@gmail.com>
2023-06-15 17:10:53 -0300

    finish the project

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-const btn = document.querySelector("get-quotes");
+const btn = document.querySelector(".get-quotes");
 btn.addEventListener("click", getQuotes);
+const number = document.getElementById("number");
+const URL = "https://type.fit/api/quotes";
 
 function getQuotes(e) {
   e.preventDefault();
 
-  fetch("users.json")
-    .then((resp) => {
-      return resp.json();
-    })
-    .then((users) => {
-      let output = "";
-      users.forEach((user) => {
-        output += `
-          <hr>
-          <ul>
-            <li>id: ${user.id}</li>
-            <li>name: ${user.name}</li>
-            <li>age: ${user.age}</li>
-            <li>email: ${user.email}</li>
-          </ul>
-          `;
+  if (number.value.length == 0) {
+    return alert("Plese enter a number");
+  } else {
+    fetch(URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(JSON.stringify(data));
+        data = shuffle(data);
+        let output = "";
+
+        for (let i = 0; i < data.length; i++) {
+          if (i == number.value) {
+            break;
+          }
+          output += `
+                    <li>Quote: ${data[i].text}</li>
+                    <li>Author: ${data[i].author}</li>
+                   <hr>
+               `;
+        }
+
+        document.querySelector(".quotes").innerHTML = output;
       });
-      document.getElementById("users").innerHTML = output;
-    });
+  }
+}
+
+//  FUNCTION TO SHUFFLE QUOTES
+function shuffle(quotes) {
+  let CI = quotes.length,
+    tempValue,
+    randomIndex;
+
+  // While elements exist in the array
+  while (CI > 0) {
+    randomIndex = Math.floor(Math.random() * CI);
+    // DECREASE CI BY 1
+    CI--;
+    // SWAP THE LAST ELEMENT WITH CI
+    tempValue = quotes[CI];
+    quotes[CI] = quotes[randomIndex];
+    quotes[randomIndex] = tempValue;
+  }
+  return quotes;
 }
